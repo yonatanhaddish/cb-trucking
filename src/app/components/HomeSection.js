4c5171e3280fd7138c85625444475d62f5202507
Yonatan Haddish <yonihailshn@gmail.com>
2025-04-25 13:19:01 -0400

    landingPage screenLessThan430 DONE

---
+++ src/app/components/HomeSection.js
@@ -0,0 +1,186 @@
+"use client";
+
+import React from "react";
+
+import { Box, Typography, useMediaQuery, Button } from "@mui/material";
+
+function HomeSection() {
+  const screenLessThan430 = useMediaQuery(
+    "(min-width: 100px) and (max-width: 430px)"
+  );
+  const screenGreaterThan430LessThan768 = useMediaQuery(
+    "(min-width: 431px) and (max-width: 768px)"
+  );
+  const screenGreaterThan768LessThan1024 = useMediaQuery(
+    "(min-width: 769px) and (max-width: 1024px)"
+  );
+  const screenGreaterThan1024LessThan1280 = useMediaQuery(
+    "(min-width: 1025px) and (max-width: 1280px)"
+  );
+  const screenGreaterThan1280LessThan1440 = useMediaQuery(
+    "(min-width: 1281px) and (max-width: 1440px)"
+  );
+  const screenGreaterThan1440LessThan1920 = useMediaQuery(
+    "(min-width: 1441px) and (max-width: 1920px)"
+  );
+  const screenGreaterThan1920LessThan3840 = useMediaQuery(
+    "(min-width: 1921px) and (max-width: 3840px)"
+  );
+  const styles = {
+    parent_landingpage: {
+      //   border: "solid red 2px",
+      backgroundImage: `url("/images/landingpage_cb.jpg")`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "86vh",
+      position: "relative",
+      display: "flex",
+      width: screenGreaterThan1920LessThan3840 ? "80%" : "100%",
+      margin: screenGreaterThan1920LessThan3840 ? "0 auto" : "",
+    },
+    overlay_box: {
+      backgroundColor: "rgba(0, 0, 0, 0.2)",
+      position: "absolute",
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+    },
+    content_landingpage: {
+      position: "relative",
+      zIndex: 1,
+      //   border: "solid green 1px",
+      width: screenLessThan430 ? "90%" : "40%",
+      display: "flex",
+      flexDirection: "column",
+      gap: "30px",
+      alignSelf: "center",
+    },
+    typo_heading: {
+      color: "#fff",
+      fontSize: "36px",
+      //   border: "solid white 1px",
+      width: "80%",
+      alignSelf: "center",
+      textAlign: "center",
+    },
+    typo_desc: {
+      color: "#fff",
+      //   border: "solid red 1px",
+      width: "80%",
+      alignSelf: "center",
+      textAlign: "center",
+    },
+    button_contact: {
+      border: "solid 1px #c23237",
+      color: "#c23237",
+      width: "150px",
+      //   fontWeight: "bold",
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+    },
+    button_services: {
+      //   border: "solid 1px white",
+      backgroundColor: "#c23237",
+      color: "#fff",
+      width: "150px",
+    },
+    button_parent: {
+      width: "80%",
+      alignSelf: "center",
+      display: "flex",
+      flexDirection: "row",
+      gap: "40px",
+      //   border: "solid 1px white",
+      justifyContent: "center",
+    },
+    parent_landing_smaller: {
+      //   border: "solid green 2px",
+      height: "86vh",
+      backgroundColor: "#424242",
+    },
+    parent_landingpage_small: {
+      backgroundImage: `url("/images/landingpage_cb.jpg")`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "50%",
+    },
+    content_landingpage_smaller: {
+      //   border: "solid red 2px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "30px",
+    },
+    typo_heading_smaller: {
+      fontSize: "34px",
+      color: "#fff",
+      //   border: "solid white 1px",
+      textAlign: "center",
+    },
+    typo_desc_smaller: {
+      color: "#fff",
+      textAlign: "center",
+    },
+    button_parent_smaller: {
+      //   border: "solid white 2px",
+      display: "flex",
+      flexDirection: "row",
+      alignSelf: "center",
+      gap: "30px",
+    },
+    button_services_smaller: {
+      backgroundColor: "#c23237",
+      color: "#fff",
+      width: "150px",
+    },
+    button_contact_smaller: {
+      border: "solid 2px #c23237",
+      color: "#c23237",
+      width: "150px",
+    },
+  };
+  return (
+    <>
+      {screenLessThan430 ? (
+        <Box sx={styles.parent_landing_smaller}>
+          <Box sx={styles.parent_landingpage_small}></Box>
+          <Box sx={styles.content_landingpage_smaller}>
+            <Typography sx={styles.typo_heading_smaller}>
+              Reliable <span style={{ color: "#c23237" }}>Trucking</span> and{" "}
+              <span style={{ color: "#c23237" }}>Transportation</span> Services
+            </Typography>
+            <Typography sx={styles.typo_desc_smaller}>
+              We provide services in the field of road transportation,ensuring
+              reliable, and efficient delivery solutions for business and
+              individuals.
+            </Typography>
+            <Box sx={styles.button_parent_smaller}>
+              <Button sx={styles.button_services_smaller}>Our Services</Button>
+              <Button sx={styles.button_contact_smaller}>Contact Us</Button>
+            </Box>
+          </Box>
+        </Box>
+      ) : (
+        <Box sx={styles.parent_landingpage}>
+          <Box sx={styles.overlay_box}></Box>
+          <Box sx={styles.content_landingpage}>
+            <Typography sx={styles.typo_heading}>
+              Reliable <span style={{ color: "#c23237" }}>Trucking</span> and{" "}
+              <span style={{ color: "#c23237" }}>Transportation</span> Services
+            </Typography>
+            <Typography sx={styles.typo_desc}>
+              We provide services in the field of road transportation,ensuring
+              reliable, and efficient delivery solutions for business and
+              individuals.
+            </Typography>
+            <Box sx={styles.button_parent}>
+              <Button sx={styles.button_services}>Our Services</Button>
+              <Button sx={styles.button_contact}>Contact Us</Button>
+            </Box>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
+}
+
+export default HomeSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
+import React from "react";
+
 import Navbar from "./components/Navbar";
+import HomeSection from "./components/HomeSection";
 
 export default function Home() {
   return (
     <>
       <>
         <Navbar />
+        <HomeSection />
       </>
     </>
   );
